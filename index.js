35bfa2863290bfd01e2e659fc93812405238d660
Tran Dan <tergitran@gmail.com>
2023-06-09 23:17:43 +0700

    add source front-end

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 
 const app = express();
 
-
+app.use(express.static('build'));
 app.use(cors());
 app.use(express.json());
 morgan.token('data-sent', function (req, res) {
